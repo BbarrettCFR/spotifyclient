ec6831267fb7083cd4bce78ef0dd52f49b71fdc8
Brandon <47222125+BbarrettCFR@users.noreply.github.com>
2019-05-24 11:22:53 -0400

    update file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import "./styles.css";
+ReactDOM.render(<App />, document.getElementById("root"));
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+serviceWorker.unregister();
